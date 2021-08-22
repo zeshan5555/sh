3ca098831661483ac322a744b87fc456f0f1b0b2
zeshan5555 <88017843+zeshan5555@users.noreply.github.com>
2021-08-22 08:40:54 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,6 @@ curl -fsSL https://code-server.dev/install.sh | sh
 cd ~/.config/code-server
 mv config.yaml llod
 wget https://raw.githubusercontent.com/hinshuika/jvjv/main/config.yaml
+apt install npm
 npm install -g localtunnel
 code-server --bind-addr 127.0.0.1:8040  & lt --port 8040
